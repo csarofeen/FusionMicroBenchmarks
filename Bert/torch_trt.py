Fix syncs in softmax trt example.

Bert/torch_trt.py
@@ -48,19 +48,21 @@ if __name__ == "__main__" :
     sumtime = 0
     for i in range(10) :
         if i>=5:
+            torch.cuda.synchronize()
             since= time.time()
         out =  jit_model.forward(inputs, mask)
         if i>=5:
+            torch.cuda.synchronize()
             sumtime += time.time() - since
-        torch.cuda.synchronize()
     print("average runtime per iter for NVFUSER:", round(sumtime*1000.0/50.0,2), 'ms')
-    trt_ts_module = torch_tensorrt.compile(jit_model,inputs=[inputs, mask],  enabled_precisions = {torch.float})
+    trt_ts_module = torch_tensorrt.compile(model,inputs=[inputs, mask],  enabled_precisions = {torch.float})
     sumtime = 0
     for i in range(10):
         if i>=5:
+            torch.cuda.synchronize()
             since= time.time()
         out =  trt_ts_module(inputs, mask)
         if i>=5:
+            torch.cuda.synchronize()
             sumtime += time.time() - since
-        torch.cuda.synchronize()
     print("average runtime per iter for TORCH-TRT:", round(sumtime*1000.0/50.0,2), 'ms')